486a0d4422d7fd59c14193193bf00517010adb1a
FlasTech <127699157+FlasTech@users.noreply.github.com>
2023-05-13 23:05:28 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+const botaoAleterarTema = document.getElementById("botao-alterar-tema");
+const body = document.querySelector("body");
+const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao");
+
+botaoAleterarTema.addEventListener("click", () => {
+  const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
+  body.classList.toggle("modo-escuro");
+  if(modoEscuroEstaAtivo){
+    imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/sun.png");
+  }
+    else{
+        imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/moon.png");
+    }
+});
